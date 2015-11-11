6d3045857d5b3525954eed2fc12969fb52903d78
ls123456789 <20141105040ls@iMac-11.local>
2015-11-11 20:47:37 +0800

    Initial Commit

---
+++ ls02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ls02
+//
+//  Created by 梁爽 on 15/11/11.
+//  Copyright (c) 2015年 梁爽. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
